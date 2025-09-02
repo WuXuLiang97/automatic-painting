a94106f009a0c039e005668a5dff2880e7edcf5d
Ke Jun Wang <wangkj@cn.ibm.com>
2025-09-02 17:43:34 +0800

    整理代码

--- scrap_code/app_0530.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QApplication
-from core.callMain import AppMain
-import sys
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    main = AppMain()
-    main.show()
-    sys.exit(app.exec())
